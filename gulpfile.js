1f1bb7d1773f2e9f22f5ef8b5451d88dae754234
vitoriamatos <vitoria.matos99@gmail.com>
2023-04-10 13:58:14 -0300

    ajustes

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ function styles(){
 function images(){
     return gulp.src('./src/images/**/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('dist/images/'));
+        .pipe(gulp.dest('dist/images'));
 }
 
 exports.default = function() {
